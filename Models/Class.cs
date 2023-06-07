1ab59b0a2facd8ca2ebcaa9aee5dc86c8a7e1b34
Adrian Lazarec <lazarec.adrian@gmail.com>
2023-06-07 16:01:39 +0300

    Add project files.

---
+++ Models/Class.cs
@@ -0,0 +1,14 @@
+﻿namespace DeviceManagerAPI.Models
+{
+    public class Class
+    {
+        public int DeviceId { get; set; }
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string OperatingSystem { get; set; }
+        public string OSVersion{ get; set; }
+        public string Processor { get; set; }
+        public int RAMAmount { get; set; }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace DeviceManagerAPI.Models
+{
+    public class Users
+    {
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string Location { get; set; }
+    }
+}
