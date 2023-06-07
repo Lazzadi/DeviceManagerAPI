05e104a2849da9ddfe8c3dca90c552ac46783dbd
Adrian Lazarec <lazarec.adrian@gmail.com>
2023-06-07 18:34:08 +0300

    created Db DataContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using DeviceManagerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeviceManagerAPI.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { 
+        
+        }
+
+        public DbSet<Devices> Devices { get; set; }
+        public DbSet<Users> Users { get; set; }
+
+
+    }
+}
